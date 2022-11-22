ca289e87e50df502e572b1f967c413653a8c670e
AndresCalvoG <andrescalvo9407@gmail.com>
2022-11-22 12:30:56 -0500

    end class 7 custom elements

---
+++ my-element.js
@@ -0,0 +1,26 @@
+const template = document.createElement("div");
+template.innerHTML = `
+<style>
+  p{
+    color:blue;
+  }
+</style>
+  <p>Hola Mundo 2</p>
+  <p>soy un Parrafo</p>
+`;
+
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+
+    this.p = document.createElement("p");
+  }
+
+  connectedCallback() {
+    this.p.textContent = "Hola Mundo";
+    this.appendChild(this.p);
+    this.appendChild(template);
+  }
+}
+
+customElements.define("my-element", myElement);
