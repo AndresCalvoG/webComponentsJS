ca0dbd6b87cf82d2d2a8d66f90830d0a8bb0189c
AndresCalvoG <andrescalvo9407@gmail.com>
2023-01-19 08:41:48 -0800

    end class 16 ::slotted

---
+++ slotted/my-element.js
@@ -0,0 +1,47 @@
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <section>
+        <h2>
+        <slot name='title'></slot>
+        </h2>
+        <p>
+          <slot name='text'></slot>
+        </p>
+        <slot></slot>
+      </section>
+      ${this.getStyles()}
+    `;
+    return template;
+  }
+
+  getStyles() {
+    return `
+    <style>
+    ::slotted(span){
+      font-size: 30px;
+      color: red;
+    }
+    ::slotted(.parrafo){
+      color:blue;
+    }
+    </style>
+    `;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define("my-element", myElement);
